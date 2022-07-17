b765ea33da1a33b010e9029f9d3fa002e0d97095
MorganNW2003 <109426059+MorganNW2003@users.noreply.github.com>
2022-07-17 23:30:03 +0200

    Update 01.hello_world.c

--- Tutorial/Programmazione/C/01.hello_world/01.hello_world.c
+++ Tutorial/Programmazione/C/01.hello_world/01.hello_world.c
@@ -8,9 +8,9 @@
 
 #include<stdio.h>  			//questa libbreria permette di utilizzare la console di input output
 
-int main()		   			//nell'int main va inserito il codice da eseguire
+int main()		   		//nell'int main va inserito il codice da eseguire
 {
-	printf("Hello World");  //printf e' la funzione che permette di stampare messaggi, stringhe o variabili
+	printf("Hello World");  	//printf e' la funzione che permette di stampare messaggi, stringhe o variabili
 	
-	return 0;				//funzione di ritono del main, quando restituisce "0" il programma non ha generato errori 
+	return 0;			//funzione di ritono del main, quando restituisce "0" il programma non ha generato errori 
 }
